a807be73668e246ca0c1ca5cd3bc542de466b408
Joscelyn James <josce.james7@hotmail.com>
2020-05-06 08:02:49 -0600

    feature: connecting to mongo

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import express from 'express';
 import graphQlHTTP from 'express-graphql';
 import schema from './src/graphql/root';
 import dotenv from 'dotenv';
+import { MongoClient } from 'mongodb';
 
 dotenv.config();
 
@@ -13,5 +14,12 @@ app.use('/graphql', graphQlHTTP({
 	schema,
 	graphiql: true,
 }));
+
+
+new MongoClient.connect(
+	process.env.MONGO_URL, 
+	{ useUnifiedTopology: true }, 
+	() => console.log('connected to mongo')
+);
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
